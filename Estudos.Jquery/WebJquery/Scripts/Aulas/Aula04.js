7a19791b432c771c4e8544c48110075fb51e6d11
Felipe Ferraroni de Melo <felipeferraroni@outlook.com>
2016-11-28 09:04:10 -0200

    Aula 04

---
+++ Estudos.Jquery/WebJquery/Scripts/Aulas/Aula04.js
@@ -0,0 +1,8 @@
+﻿/* Aula 04 de Java Script */
+
+$(document).ready(function () {
+        $("#titulo").css("color", "#0000FF");
+        $("a").css("color", "#00FF00");
+        $(".outros").css("color", "#FF0000");
+        $("h1, a").css("background-color", "#ffb6c1");
+});
